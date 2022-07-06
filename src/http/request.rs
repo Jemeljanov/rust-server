d915994095432a7110dc6dbd70fa53fd66a31799
Jemeljanov <jemeljanov.dmitri@gmail.com>
2022-07-07 01:29:24 +0200

    added lifetimes

--- src/http/request.rs
+++ src/http/request.rs
@@ -6,17 +6,17 @@ use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
 use std::str::Utf8Error;
 
 
-pub struct Request {
-    path : String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path : &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -35,7 +35,11 @@ impl TryFrom<&[u8]> for Request {
             path = &path[..i];
         }
 
-        unimplemented!()
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })       
     }
 }
 
