3b8afeaee74a1f6c59a2971bc18b0ba3534ca8be
Jemeljanov <jemeljanov.dmitri@gmail.com>
2022-07-07 13:36:28 +0200

    server response

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 use std::convert::TryFrom;
-use crate::http::Request;
-use std::io::Read;
+use std::io::{Write, Read};
+use crate::http::{Request, Response, StatusCode};
 
 pub struct Server {
     addr: String,
@@ -25,12 +25,21 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
 
-                            match Request::try_from(&buffer[..]) {
+                            let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    Response::new(StatusCode::Ok, Some("<h1>It works!!!</h1>".to_string()))
                                 },
-                                Err(e) => println!("Failed to parse the request: {}", e),
+                                Err(e) => {
+                                    println!("Failed to parse the request: {}", e);
+                                    Response::new(StatusCode::BadRequest, None)
+                                },
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
+
                         }
                         Err(e) => println!("Failed to read from connection {}", e),
                     }
