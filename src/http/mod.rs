d70d817744d986f2f7b76ab6bb0f9eb01d2c1092
Jemeljanov <jemeljanov.dmitri@gmail.com>
2022-07-07 12:42:50 +0200

    added response

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,7 +2,9 @@ pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use response::Response;
 
 pub mod request;
 pub mod method;
 pub mod query_string;
+pub mod response;
