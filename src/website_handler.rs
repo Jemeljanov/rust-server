0adf2ae8257250d3d6dd7dfe30584cfea3b8abfb
Jemeljanov <jemeljanov.dmitri@gmail.com>
2022-07-08 13:55:48 +0200

    removed bug

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -12,20 +12,10 @@ impl WebsiteHandler {
     }
 
     fn read_file(&self, file_path: &str) -> Option<String> {
-        let path = format!("{}/{}", self.public_path, file_path);
+        let path = format!("{}\\{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
  
-        //edit this match
-        match fs::canonicalize(path) {
-            Ok(path) => {
-                if path.starts_with(&self.public_path) {
-                    fs::read_to_string(path).ok()
-                } else {
-                    println!("Directory Traversal Attack Attempted: {}", file_path);
-                    None
-                }
-            }
-            Err(_) => None,
-        }
+        //Directory traversal attack not prevented
     }
 }
 
